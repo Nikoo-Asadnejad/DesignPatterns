eaeddc256a52c0b6423040c0915ac92b0d490d5c
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2024-07-26 11:09:08 +0330

    feat: add adapter design pattern

--- Src/DesignPatterns/Structural/Adapter.cs
+++ Src/DesignPatterns/Structural/Adapter.cs
@@ -1,6 +1,30 @@
 namespace DesignPatterns;
 
-public class Adapter
+public interface ITarget
 {
-    
+    string GetRequest();
+}
+
+public class Adaptee
+{
+    public string GetSpecificRequest()
+    {
+        return "Specific request";
+    }
+}
+
+public class Adapter : ITarget
+{
+    private readonly Adaptee _adaptee;
+
+    public Adapter(Adaptee adaptee)
+    {
+        _adaptee = adaptee;
+    }
+
+    public string GetRequest()
+    {
+        // Translate the request to the adaptee's method
+        return $"This is '{_adaptee.GetSpecificRequest()}'";
+    }
 }
