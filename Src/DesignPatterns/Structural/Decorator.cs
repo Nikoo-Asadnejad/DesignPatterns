ace526b1c916aa74f9cef962675fb378d626052b
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2024-07-26 10:52:49 +0330

    feat: add decorator pattern

--- Src/DesignPatterns/Structural/Decorator.cs
+++ Src/DesignPatterns/Structural/Decorator.cs
@@ -1,6 +1,38 @@
 namespace DesignPatterns;
 
-public class Decorator
+
+public interface IRepository
 {
-    
+    string Get();
 }
+
+public class Repository : IRepository
+{
+    public string Get()
+    {
+        return "ConcreteComponent";
+    }
+}
+
+public abstract class RepositoryCacheDecorator : IRepository
+{
+    protected IRepository _repository;
+
+    public RepositoryCacheDecorator(IRepository repository )
+    {
+        _repository = repository;
+    }
+
+    public virtual string Get()
+    {
+        //exist in cache return
+       
+        //else
+        return _repository.Get();
+    }
+}
+
+
+
+
+
