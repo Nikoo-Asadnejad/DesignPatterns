64d634d7ddf740ed77769456ecc20cf68f369a82
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2024-07-26 11:20:03 +0330

    feat: add strategy design pattern

--- Src/DesignPatterns/Behavioral/Strategy.cs
+++ Src/DesignPatterns/Behavioral/Strategy.cs
@@ -1,6 +1,43 @@
 namespace DesignPatterns;
 
-public class Strategy
+public interface IStrategy
 {
-    
+    void Execute();
 }
+
+public class StrategyA : IStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Strategy A Execution");
+    }
+}
+
+public class StrategyB : IStrategy
+{
+    public void Execute()
+    {
+        Console.WriteLine("Strategy B Execution");
+    }
+}
+
+public class Context
+{
+    private IStrategy _strategy;
+
+    public Context(IStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void SetStrategy(IStrategy strategy)
+    {
+        _strategy = strategy;
+    }
+
+    public void ExecuteStrategy()
+    {
+        _strategy.Execute();
+    }
+}
+
