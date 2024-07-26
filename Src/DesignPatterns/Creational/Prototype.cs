78b1be493d8bbcdde4639d70a8d518f135fe88e0
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2024-07-26 10:45:26 +0330

    feat: add prototype pattern

--- Src/DesignPatterns/Creational/Prototype.cs
+++ Src/DesignPatterns/Creational/Prototype.cs
@@ -1,6 +1,42 @@
-namespace DesignPatterns;
+namespace DesignPatterns.Creational;
 
-public class Prototype
+public interface IPrototype
 {
+    IPrototype Clone();
+}
+
+public class ProductModel : IPrototype
+{
+    public string Name { get; set; }
+
+    public ProductModel(string name)
+    {
+        Name = name;
+    }
+
+    public IPrototype Clone()
+    {
+        // Create a shallow copy
+        return (IPrototype)this.MemberwiseClone();
+    }
     
 }
+
+public class CategoryModel : IPrototype
+{
+    public int Type { get; set; }
+
+    public CategoryModel(int value)
+    {
+        Type = value;
+    }
+
+    public IPrototype Clone()
+    {
+        // Create a shallow copy
+        return (IPrototype)this.MemberwiseClone();
+    }
+    
+}
+
+
