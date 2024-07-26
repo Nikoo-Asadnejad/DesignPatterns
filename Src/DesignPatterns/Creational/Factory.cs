0efa7e51f3ae10a6b8e1ac08087c4aa451616038
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2024-07-26 10:17:55 +0330

    feat: add factory pattern

--- Src/DesignPatterns/Creational/Factory.cs
+++ Src/DesignPatterns/Creational/Factory.cs
@@ -1,6 +1,38 @@
 namespace DesignPatterns.Creational;
 
-public class Factory
+public interface IProductService
 {
-    
+    string GetProduct();
+}
+
+public class ProductServiceA : IProductService
+{
+    public string GetProduct()
+    {
+        return "ConcreteProductA";
+    }
+}
+
+public class ProductServiceB : IProductService
+{
+    public string GetProduct()
+    {
+        return "ConcreteProductB";
+    }
+}
+
+public static class ProductFactory
+{
+    public static IProductService CreateProduct(string productType)
+    {
+        switch (productType.ToLower())
+        {
+            case "a":
+                return new ProductServiceA();
+            case "b":
+                return new ProductServiceB();
+            default:
+                throw new ArgumentException("Invalid product type", nameof(productType));
+        }
+    }
 }
