6f897599d1f044cceaf793341b23eb044f6d4523
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2024-07-26 10:56:55 +0330

    feat: add proxy pattern

--- Src/DesignPatterns/Structural/Proxy.cs
+++ Src/DesignPatterns/Structural/Proxy.cs
@@ -1,6 +1,32 @@
 namespace DesignPatterns;
 
-public class Proxy
+public interface IRequestHandler
 {
-    
+    void Send();
 }
+
+public class RequestHandler : IRequestHandler
+{
+    public void Send()
+    {
+        Console.WriteLine("RealSubject: Sending Request.");
+    }
+}
+
+public class RequestHandlerProxy : IRequestHandler
+{
+    private RequestHandler _realSubject;
+    public void Send()
+    {
+        if (_realSubject == null)
+        {
+            _realSubject = new RequestHandler();
+        }
+        
+        //Log Request Here
+        Console.WriteLine("Proxy: Logging Request before forwarding to RequestHandler.");
+        
+        _realSubject.Send();
+    }
+}
+
