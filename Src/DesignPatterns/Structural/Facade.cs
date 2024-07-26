45e11799df1ff5bfd71b9cbd6b4452e3d5dad34c
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2024-07-26 11:01:17 +0330

    feat: add facade design pattern

--- Src/DesignPatterns/Structural/Facade.cs
+++ Src/DesignPatterns/Structural/Facade.cs
@@ -1,6 +1,54 @@
 namespace DesignPatterns;
 
-public class Facade
+public sealed class SubsystemA
 {
-    
+    public void OperationA()
+    {
+        Console.WriteLine("SubsystemA: OperationA");
+    }
 }
+
+public sealed class SubsystemB
+{
+    public void OperationB()
+    {
+        Console.WriteLine("SubsystemB: OperationB");
+    }
+}
+
+public sealed class SubsystemC
+{
+    public void OperationC()
+    {
+        Console.WriteLine("SubsystemC: OperationC");
+    }
+}
+
+public sealed class Facade
+{
+    private readonly SubsystemA _subsystemA;
+    private readonly SubsystemB _subsystemB;
+    private readonly SubsystemC _subsystemC;
+
+    public Facade()
+    {
+        _subsystemA = new SubsystemA();
+        _subsystemB = new SubsystemB();
+        _subsystemC = new SubsystemC();
+    }
+
+    public void Operation1()
+    {
+        Console.WriteLine("Facade: Operation1");
+        _subsystemA.OperationA();
+        _subsystemB.OperationB();
+    }
+
+    public void Operation2()
+    {
+        Console.WriteLine("Facade: Operation2");
+        _subsystemB.OperationB();
+        _subsystemC.OperationC();
+    }
+}
+
