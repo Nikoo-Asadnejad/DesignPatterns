c194c6b521e7d2db1cc82805323a4f2efe2c4680
Nikoo Asadnejad <nikoo.a.14@gmail.com>
2024-07-26 10:33:06 +0330

    feat: add abstract factory pattern

--- Src/DesignPatterns/Creational/AbstractFactory.cs
+++ Src/DesignPatterns/Creational/AbstractFactory.cs
@@ -1,6 +1,75 @@
 namespace DesignPatterns.Creational;
 
-public class AbstractFactory
+public interface ISupplierServiceA
 {
-    
+    string GetName();
+}
+
+public interface ISupplierServiceB
+{
+    string GetDescription();
+}
+
+public class SupplierServiceASoap : ISupplierServiceA
+{
+    public string GetName()
+    {
+        return "ConcreteProductA1";
+    }
+}
+
+public class SupplierServiceARest : ISupplierServiceA
+{
+    public string GetName()
+    {
+        return "ConcreteProductA2";
+    }
+}
+
+public class SupplierServiceBSoap : ISupplierServiceB
+{
+    public string GetDescription()
+    {
+        return "Description of ConcreteProductB1";
+    }
+}
+
+public class SupplierServiceBRest : ISupplierServiceB
+{
+    public string GetDescription()
+    {
+        return "Description of ConcreteProductB2";
+    }
+}
+
+public interface ISupplierFactory
+{
+    ISupplierServiceA CreateSupplierServiceA();
+    ISupplierServiceB CreateSupplierServiceB();
+}
+
+public sealed class SoapSupplierFactory : ISupplierFactory
+{
+    public ISupplierServiceA CreateSupplierServiceA()
+    {
+        return new SupplierServiceASoap();
+    }
+
+    public ISupplierServiceB CreateSupplierServiceB()
+    {
+        return new SupplierServiceBSoap();
+    }
+}
+
+public class RestSupplierFactory : ISupplierFactory
+{
+    public ISupplierServiceA CreateSupplierServiceA()
+    {
+        return new SupplierServiceARest();
+    }
+
+    public ISupplierServiceB CreateSupplierServiceB()
+    {
+        return new SupplierServiceBRest();
+    }
 }
